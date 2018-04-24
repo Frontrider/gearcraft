c31b5eb85231819aff7f3832dc8cb953af252773
András Kis <kisandrasgabor@gmail.com>
2018-04-24 22:26:00 +0200

    updated API documentation

--- src/main/java/hu/frontrider/gearcraft/api/PoweredBlock.java
+++ src/main/java/hu/frontrider/gearcraft/api/PoweredBlock.java
@@ -5,9 +5,20 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
+/**
+ * This interface have to be implemented by every block, which can output power.
+ * */
 public interface PoweredBlock {
-
+    /**
+     * Returns the signal strength of the block
+     * */
     int getPower(IBlockAccess iBlockAccess, BlockPos blockPos, IBlockState leftBlock);
+    /**
+     * Returns the amount of power it can transfer
+     * */
     int getStrength(IBlockAccess iBlockAccess, BlockPos blockPos);
+    /**
+     * Checks if it can transfer power on this side.
+     * */
     boolean isValidSide(IBlockAccess access, BlockPos pos, EnumFacing facing);
 }
